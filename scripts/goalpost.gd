902e3a8468761e3fec6c5d1fa77c69bf34ed6d56
Eeli Ogata <eeli.ogata@gmail.com>
2026-02-01 13:20:14 +0200

    Added goal

---
+++ scripts/goalpost.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+signal goal_reached
+
+@export var next_level: String = ""
+
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		goal_reached.emit()
+		
+		if next_level.is_empty():
+			get_tree().change_scene_to_file("res://scenes/victorymenu.tscn")
+		else:
+			get_tree().change_scene_to_file(next_level)
