a17200dd40d2a878ed4b86311573fa48933e4404
Eeli Ogata <eeli.ogata@gmail.com>
2026-02-01 14:15:12 +0200

    Fixed scaling

--- scripts/resolutionsautomatic.gd
+++ scripts/resolutionsautomatic.gd
@@ -1,7 +1,32 @@
 extends OptionButton
 
+var resolutions = [
+	Vector2i(1920, 1080),
+	Vector2i(1600, 900),
+	Vector2i(1280, 720),
+	Vector2i(960, 540)
+]
+
+
 func _ready():
 	clear()
-	add_item("1920 x 1080")
-	add_item("1600 x 900")
-	add_item("1280 x 720")
+	for res in resolutions:
+		add_item("%d x %d" % [res.x, res.y])
+	
+	# Select current resolution
+	var current_size = DisplayServer.window_get_size()
+	for i in range(resolutions.size()):
+		if resolutions[i] == current_size:
+			select(i)
+			break
+	
+	item_selected.connect(_on_resolution_selected)
+
+
+func _on_resolution_selected(index: int) -> void:
+	var new_res = resolutions[index]
+	DisplayServer.window_set_size(new_res)
+	# Center the window
+	var screen_size = DisplayServer.screen_get_size()
+	var window_pos = (screen_size - new_res) / 2
+	DisplayServer.window_set_position(window_pos)
